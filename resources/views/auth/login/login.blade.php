c82a0491ae563130e51a1869492f5dbdc6c9a0d2
嘉川素子 <19motoko980430@gmail.com>
2024-10-12 22:49:57 +0900

    ログイン画面レイアウト

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -16,34 +16,49 @@
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 
-<body>
+<body style="background-color:#E8F0F7">
     <form action="{{ route('loginPost') }}" method="POST">
-        <a style="align-items:center"><img src="../../../../public/image/atlas-black.png" width="240" height="180"
-                alt=""></a>
-        <br>
         <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-
-            <div class="border vh-50 w-25">
-                <div class="w-75 m-auto pt-5">
-                    <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
-                    <div class="border-bottom border-primary w-100">
-                        <input type="text" class="w-100 border-0" name="mail_address">
+            {{-- 1012 add --}}
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col w-50 vh-50">
+                        <img src="/image/atlas-black.png" style="align-items:center; justify-content:center;"
+                            width="200" height="80">
                     </div>
                 </div>
-                <div class="w-75 m-auto pt-5">
-                    <label class="d-block m-0" style="font-size:13px;">パスワード</label>
-                    <div class="border-bottom border-primary w-100">
-                        <input type="password" class="w-100 border-0" name="password">
+                <div class="row">
+                    <div class="col">
                     </div>
-                </div>
-                <div class="text-right m-3">
-                    <input type="submit" class="btn btn-primary" value="ログイン">
-                </div>
-                <div class="text-center">
-                    <a href="{{ route('registerView') }}">新規登録</a>
+                    <div class="col">
+                        {{-- <div class="border vh-50 w-25"> --}}
+                        <div
+                            style="border: 1px solid #cccccc; margin-top:40px; padding-bottom:20px; box-shadow: 4px 4px 8px #dddddd; border-radius: 10px; background-color:white">
+                            <div class="w-75 m-auto pt-5">
+                                <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
+                                <div class="border-bottom border-primary w-100">
+                                    <input type="text" class="w-100 border-0" name="mail_address">
+                                </div>
+                            </div>
+                            <div class="w-75 m-auto pt-5">
+                                <label class="d-block m-0" style="font-size:13px;">パスワード</label>
+                                <div class="border-bottom border-primary w-100">
+                                    <input type="password" class="w-100 border-0" name="password">
+                                </div>
+                            </div>
+                            <div class="text-right m-3">
+                                <input type="submit" class="btn btn-primary" value="ログイン">
+                            </div>
+                            <div class="text-center">
+                                <a href="{{ route('registerView') }}">新規登録はこちら</a>
+                            </div>
+                            {{ csrf_field() }}
+                        </div>
+                    </div>
+                    <div class="col">
+                    </div>
                 </div>
             </div>
-            {{ csrf_field() }}
         </div>
     </form>
     </div>
