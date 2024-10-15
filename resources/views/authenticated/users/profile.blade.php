d20c5244232315d3d65e2019617df3671c475bcd
嘉川素子 <19motoko980430@gmail.com>
2024-10-15 22:21:17 +0900

    アコーディオンメニューテスト２

--- resources/views/authenticated/users/profile.blade.php
+++ resources/views/authenticated/users/profile.blade.php
@@ -18,32 +18,8 @@
                     @endforeach
                 </div>
                 <div class="">
-                    {{-- 1015 add --}}
-                    <div class="accordion" id="accordionExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Accordion Item #1
-                                </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show"
-                                data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until
-                                    the collapse plugin adds the appropriate classes that we use to style each element.
-                                    These classes control the overall appearance, as well as the showing and hiding via CSS
-                                    transitions. You can modify any of this with custom CSS or overriding our default
-                                    variables. It's also worth noting that just about any HTML can go within the
-                                    <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- 1015 add --}}
-
                     @can('admin')
-                        <span class="subject_edit_btn">選択科目の編集</span>
+                        <span class="subject_edit_btn">選択科目の編集<span class="inn"></span></span>
                         <div class="subject_inner">
                             <form action="{{ route('user.edit') }}" method="post">
                                 @foreach ($subject_lists as $subject_list)
