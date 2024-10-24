e50f5e13d231287956218db00070efcad87d46fd
嘉川素子 <19motoko980430@gmail.com>
2024-10-24 20:05:10 +0900

    初期データ登録、デバックバー導入

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,5 +13,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
+           DB::table('users')->insert([
+            ['over_name' => '田中',
+            'under_name' => '七郎',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'シチロウ',
+            'mail_address' => 'shichiro@com',
+            'birth_day' => '20050615',
+            'sex' => 01,
+            'role' => 03,
+            'password' => bcrypt('12345678')]
+        ]);
     }
 }
